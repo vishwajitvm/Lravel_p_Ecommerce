d6176fa8e8554fb3bda92c1c1a97fd9eb43b225f
vishwajitvm <vishwajitmall50@gmail.com>
2022-10-01 22:58:13 +0530

    Delete Cart Item

--- app/Http/Livewire/CartComponent.php
+++ app/Http/Livewire/CartComponent.php
@@ -19,6 +19,17 @@ class CartComponent extends Component
         Cart::update($rowId , $qty) ;
     }
 
+    public function destroy($rowId) {
+        Cart::remove($rowId) ;
+        session()->flash('success_message' , 'Item Has been Removed') ;
+    }
+
+    public function destroyAll() {
+        Cart::destroy() ;
+        session()->flash('success_message' , 'All Item Has been Removed from the cart') ;
+    }
+
+
 
     public function render()
     {
